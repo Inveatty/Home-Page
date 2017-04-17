0db890a37566276df4ddd37c2518d3f3aabf66b9
Dauzy <daniel_6287@hotmail.com>
2017-04-16 20:42:56 -0500

    Primeras animaciones

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
+  $('.scrollspy').scrollSpy({
+    scrollOffset:0
+  });
 });
+
+var options = [
+  {selector: '#staggered', offset: 500, callback: function(el){
+    Materialize.showStaggeredList($(el));
+    $("#call-1").velocity({ backgroundColor: "#333", color: "#ef5350" }, {duration: 500});
+  } },
+  {selector: '#staggered2', offset: 500, callback: function() {
+    Materialize.showStaggeredList($(el));
+    $("#call-2").velocity({ backgroundColor: "#333", color: "#ef5350" }, {duration: 500})
+  } },
+];
+Materialize.scrollFire(options);
